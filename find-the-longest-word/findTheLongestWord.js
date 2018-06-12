dab3e009fbb5423ff409adc985908418b6da61ff
DLeaguer <immostlikely@hotmail.com>
2018-06-12 11:32:00 -1000

    completed find longest word & stray value

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,14 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+  var longest = '';
+  for (var i = 0; i < sen.length; i++) {
+    if (longest.length > sen[i].length) {
+      lengest =longest;
+    }
+    else {
+      longest = sen[i];
+    }
+  }
+  console.log(longest);
+  return longest;
 }
--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,9 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== array[i + 1]) {
+      console.log(array[i + 1]);
+      return array[i + 1];
+    }
+  }
 }
