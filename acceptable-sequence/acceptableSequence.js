5466bd34d5f0be8bf2121b7b2125f3c176085fd3
DLeaguer <immostlikely@hotmail.com>
2018-06-11 18:09:20 -1000

    fixed acceptableSequence.js to match all instead of just one

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,15 +1,16 @@
 module.exports = function acceptableSequence(str) {
   // write your code here
-  var letter = str.match(/[a-z|A-Z]/i);
-  console.log(letter);
-   var i = str.indexOf(letter);
-   console.log(str[i]);
-   if (str[i - 1] && str[i + 1] === '+') {
-      return true;
-    } else {
-      return false;
+  for (var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (str[i].match(/[a-z]/gi)) {
+      // console.log(str[i]);
+      if ( str[i-1] !== '+' || str[i+1] !== '+') {
+        // console.log(str[i]);
+        return false;
+      }
     }
-    console.log(i);
   }
+  return true;
+}
 
 
