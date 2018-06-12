891a2d699084181068c43344233696a85e687e2c
DLeaguer <immostlikely@hotmail.com>
2018-06-11 19:47:07 -1000

    completed findMiddleValue.js

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,24 +1,35 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
-var lowest = 0;
-var highest = 0;
-for (var i = 0; i < numArray.length -1; i++) {
-    if ( numArray[i] < numArray[i + 1] && numArray[i + 2]) {
-      lowest += numArray[i];
-      console.log(lowest);
-    } else {
-      highest = numArray[i];
-      console.log(highest);
+  console.log(numArray);
+  var largest = 0;
+  var smallest = 0;
+  for (var i = 0; i < numArray.length; i++) {
+    console.log('largest ' + largest);
+    if (largest > numArray[i]) {
+      largest = largest;
     }
-}
-for (var j = 0; j < numArray.length; j++) {
-    if (numArray[i] !== lowest && numArray[i] !== highest) {
-      console.log(i);
-      return i;
+    else {
+      largest = numArray[i];
     }
-}
+  }
+  for (var j = 0; j < numArray.length; j++) {
+    smallest = numArray[j];
+  }
+  for (var l = 0; l < numArray.length; l++) {
+    console.log('smallest ' + smallest);
+    if (smallest < numArray[l]) {
+      smallest = smallest;
+    }
+    else {
+      smallest = numArray[l];
+    }
+  }
+  for (var k = 0; k < numArray.length; k++) {
+    if (numArray[k] !== largest && numArray[k] !== smallest) {
+      console.log('numArray[k] ' + numArray[k]);
+      console.log('k ' + k);
+      return k;
+    }
+  }
 
-  //find lowest var lowest
-  //find highest var highest
-  //if numArray !== lowest || highest return i && numArray[i]
 }
