eb7d76b474998ba3083000fa657110eacd844246
DLeaguer <immostlikely@hotmail.com>
2018-06-19 14:09:52 -1000

    completed up to insert-dashes excluding even-ladder-pattern

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,25 @@
 module.exports = function insertDashes(num) {
   // write your code in here
+  console.log(num);
+  var str = '' + num;
+  var newStr = '';
+  console.log(str);
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] % 2 === 1) {
+      newStr += str[i];
+      console.log('  odd  ', str[i]);
+      console.log('  odd newStr  ', newStr);
+    }
+    if (str[i] % 2 === 0 && str[i+1] % 2 === 0) {
+      newStr += str[i] + '-';
+      console.log('  even & even  ', str[i] + '-' + str[i + 1]);
+      console.log('  even & even newStr  ', newStr);
+    }
+    else if (str[i] % 2 === 0) {
+      newStr += str[i];
+      console.log('  even  ', str[i]);
+      console.log('  even newStr  ', newStr);
+    }
+  }
+  return newStr;
 }
