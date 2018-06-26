413e0d5abe66661438da4226cc1a162c8a460a5e
DLeaguer <immostlikely@hotmail.com>
2018-06-25 19:24:51 -1000

    completed needle in haystack

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,12 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
+  for (var i = 0; i < haystack.length; i++) {
+    if (haystack[i] === 'needle') {
+      console.log('  haystack[i]  ', haystack[i]);
+      console.log('  i  ', i);
+      return i;
+    }
+  }
+  console.log('false');
+  return false;
 }
